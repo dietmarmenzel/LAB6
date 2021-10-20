3e595623988ea71f49be28e97d403f119cc88d3b
dietmarmenzel <89163030+dietmarmenzel@users.noreply.github.com>
2021-10-20 00:55:02 -0600

    definicion de pines

--- main.cpp
+++ main.cpp
@@ -11,3 +11,16 @@
 //Librería para la pantalla LCD
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+
+//*****************************************************************************
+// Definición de pines
+//*****************************************************************************
+//Definición de pines para la pantalla LCD
+#define d4 14
+#define d5 27
+#define d6 26
+#define d7 25
+#define en 12
+#define rs 13
+//Definición del pin para el potenciómetro
+#define pot1 34
